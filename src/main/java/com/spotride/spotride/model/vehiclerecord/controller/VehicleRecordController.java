75ce19cf92fa06d8c818efbcd29c0d9d9ee14c53
Andrey Shipulya <andrey.shipulya@vizor-games.com>
2024-10-15 22:15:47 +0200

    VehicleRecord service, controller, repository, mapper

--- src/main/java/com/spotride/spotride/model/vehiclerecord/controller/VehicleRecordController.java
+++ src/main/java/com/spotride/spotride/model/vehiclerecord/controller/VehicleRecordController.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
