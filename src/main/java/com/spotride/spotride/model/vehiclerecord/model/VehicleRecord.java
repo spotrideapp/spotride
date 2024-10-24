8697c85d3b99d752cb25d249b5668c4d03f9d1e1
Andrey Shipulya <andrey.shipulya@vizor-games.com>
2024-10-24 22:19:43 +0200

    liquibase changesets fixed

--- src/main/java/com/spotride/spotride/model/vehiclerecord/model/VehicleRecord.java
+++ src/main/java/com/spotride/spotride/model/vehiclerecord/model/VehicleRecord.java
@@ -9,7 +9,6 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -39,8 +38,7 @@ public class VehicleRecord {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
-    @JoinColumn(name = "vehicle_record_id")
+    @OneToMany(mappedBy = "vehicleRecord", cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
     @JsonManagedReference
     private List<VehiclePhoto> vehiclePhotos;
 
