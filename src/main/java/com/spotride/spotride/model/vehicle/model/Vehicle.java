73f918f96fba0bea33205b3a900dc58e314cf56f
Andrey Shipulya <andrey.shipulya@vizor-games.com>
2024-11-03 16:12:20 +0100

    Vehicle

---
+++ src/main/java/com/spotride/spotride/model/vehicle/model/Vehicle.java
@@ -0,0 +1,78 @@
+package com.spotride.spotride.model.vehicle.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.spotride.spotride.model.user.model.User;
+import com.spotride.spotride.model.vehiclerecord.model.VehicleRecord;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * Vehicle model.
+ */
+@Data
+@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "vehicles")
+public class Vehicle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    @JsonBackReference
+    private User user;
+
+    private String brand;
+
+    private String model;
+
+    private String generation;
+
+    private int productYear;
+
+    private String bodyType;
+
+    private String engineType;
+
+    private int enginePower;
+
+    private int engineDisplacement;
+
+    private String vehiclePhotoUrl;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
+    @JoinColumn(name = "vehicle")
+    @JsonManagedReference
+    private List<VehicleRecord> vehicleRecords;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+}
