2b3521860f9896b32fbee2651d0d078551e250eb
Andrey Shipulya <andrey.shipulya@vizor-games.com>
2025-01-21 21:07:37 +0100

    Revert "Facade for VehiclePhoto"
    This reverts commit bffd88f0f016eaa1334b6b647448de280732bd89.

--- src/test/java/com/spotride/spotride/model/vehiclephoto/VehiclePhotoControllerTest.java
+++ src/test/java/com/spotride/spotride/model/vehiclephoto/VehiclePhotoControllerTest.java
@@ -3,6 +3,7 @@ package com.spotride.spotride.model.vehiclephoto;
 import com.spotride.spotride.model.vehiclephoto.controller.VehiclePhotoController;
 import com.spotride.spotride.model.vehiclephoto.dto.VehiclePhotoResponseDto;
 import com.spotride.spotride.model.vehiclephoto.dto.request.VehiclePhotoUpdateRequestDto;
+import com.spotride.spotride.model.vehiclephoto.service.VehiclePhotoService;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -27,7 +28,7 @@ import static org.mockito.Mockito.when;
 class VehiclePhotoControllerTest {
 
     @MockBean
-    private VehiclePhotoFacade mockVehiclePhotoFacade;
+    private VehiclePhotoService mockVehiclePhotoService;
 
     @Autowired
     private VehiclePhotoController vehiclePhotoController;
@@ -39,13 +40,13 @@ class VehiclePhotoControllerTest {
                 .url("http://test.url")
                 .build();
 
-        when(mockVehiclePhotoFacade.getAll()).thenReturn(List.of(vehiclePhotoDto));
+        when(mockVehiclePhotoService.getAll()).thenReturn(List.of(vehiclePhotoDto));
 
         var result = vehiclePhotoController.getAll();
 
         assertNotNull(result);
         assertEquals(1, result.size());
-        verify(mockVehiclePhotoFacade, times(1)).getAll();
+        verify(mockVehiclePhotoService, times(1)).getAll();
     }
 
     @Test
@@ -55,14 +56,14 @@ class VehiclePhotoControllerTest {
                 .url("http://test.url")
                 .build();
 
-        when(mockVehiclePhotoFacade.getById(1L)).thenReturn(vehiclePhotoDto);
+        when(mockVehiclePhotoService.getById(1L)).thenReturn(vehiclePhotoDto);
 
         var response = vehiclePhotoController.getById(1L);
 
         assertNotNull(response);
         assertEquals(200, response.getStatusCode().value());
         assertEquals("http://test.url", Objects.requireNonNull(response.getBody()).getUrl());
-        verify(mockVehiclePhotoFacade, times(1)).getById(1L);
+        verify(mockVehiclePhotoService, times(1)).getById(1L);
     }
 
     @Test
@@ -77,24 +78,24 @@ class VehiclePhotoControllerTest {
                 .url("http://updated.url")
                 .build();
 
-        when(mockVehiclePhotoFacade.update(1L, vehiclePhotoUpdateRequestDto)).thenReturn(updatedVehiclePhotoDto);
+        when(mockVehiclePhotoService.update(1L, vehiclePhotoUpdateRequestDto)).thenReturn(updatedVehiclePhotoDto);
 
         var response = vehiclePhotoController.update(1L, vehiclePhotoUpdateRequestDto);
 
         assertNotNull(response);
         assertEquals(200, response.getStatusCode().value());
         assertEquals("http://updated.url", Objects.requireNonNull(response.getBody()).getUrl());
-        verify(mockVehiclePhotoFacade, times(1)).update(1L, vehiclePhotoUpdateRequestDto);
+        verify(mockVehiclePhotoService, times(1)).update(1L, vehiclePhotoUpdateRequestDto);
     }
 
     @Test
     void testDeleteVehiclePhoto() {
-        doNothing().when(mockVehiclePhotoFacade).delete(1L);
+        doNothing().when(mockVehiclePhotoService).delete(1L);
 
         var response = vehiclePhotoController.delete(1L);
 
         assertNotNull(response);
         assertEquals(204, response.getStatusCode().value());
-        verify(mockVehiclePhotoFacade, times(1)).delete(1L);
+        verify(mockVehiclePhotoService, times(1)).delete(1L);
     }
 }
